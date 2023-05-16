c4ea0691e59d5197bb0c4eb6d66ec64d62ffd753
Lil-MiC <128043870+Lil-MiC@users.noreply.github.com>
2023-05-16 15:10:25 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     
-    css: [
-        // Load a Node.js module directly (here it's a Sass file)
-        'bulma',
-        // CSS file in the project
-        '~/assets/css/main.css',
-        // SCSS file in the project
-        '~/assets/css/main.scss'
-        
-      ]
     
     
   
